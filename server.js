ed3565b7f4213619f3b3637030d610018fe5d465
Sandi <sandi.c.gau@gmail.com>
2019-01-13 19:05:47 -0600

    returning all responses

--- server.js
+++ server.js
@@ -6,23 +6,18 @@ require('dotenv').config();
 
 const client = yelp.client(process.env.API_KEY);
 
-const getRandom = max => {
-  return Math.floor(Math.random() * Math.floor(max));
-}
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
-app.get('/getResults/:restaurant/:location/:limit/:radius/:price/:open_now', (req,res) => client.search(req.params).then(response => {
-    let x = getRandom(50)
-    const result = response.jsonBody.businesses[x];
-    const prettyJson = JSON.stringify(result, null, 4);
-    res.send(prettyJson)
-  }).catch(e => {
-    console.log(e);
-  }))
+app.get('/getResults/:restaurant/:location/:limit/:radius/:price/:open_now', (req, res) => client.search(req.params).then(response => {
+  const result = response.jsonBody.businesses;
+  const prettyJson = JSON.stringify(result, null, 4);
+  res.send(prettyJson)
+}).catch(e => {
+  console.log(e);
+}))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
